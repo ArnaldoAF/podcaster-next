ac519aa824c0a8ebfbb7c577fdab117b8b57cba4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-04-20 22:21:28 -0300

    consumindo Api na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+// 
 
 
-
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
   return (
-    <h1>INDEXX</h1> 
+    <div>
+      <h1>INDEXX</h1> 
+
+    </div>
     
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data =  await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
